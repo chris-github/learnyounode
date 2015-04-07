8ab9d68976695ee4c2043efa9cd80b874c025d36
Chris <chris@example.com>
2015-04-08 00:57:45 +0200

    7th challange

---
+++ 07-http-client/program.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var url = process.argv[2];
+
+var req = http.get(url, function(res){
+	res.setEncoding('utf8');
+	res.on('data', function(data){
+		console.log(data);
+	});
+
+	res.on('error', function(err){
+		console.error('Error:' + err);
+	})
+});
